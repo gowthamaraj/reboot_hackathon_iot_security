c5c26107aefe0014c3d1ade4f9858687a20f81a9
gowthamaraj <gowthamaraj360@gmail.com>
2019-03-06 23:54:38 +0530

    scapy code written to detect the packets

---
+++ monitor.py
@@ -0,0 +1,15 @@
+from scapy.all import * 
+def sniffPackets(packet):
+    if packet.haslayer(IP):
+        pckt_src=packet[IP].src
+        pckt_dst=packet[IP].dst
+        pckt_ttl=packet[IP].ttl
+        print('IP Packet: ',pckt_src,' is going to ',pckt_dst,' and has ttl value ',pckt_ttl)
+
+def main():
+    print("custom packet sniffer")
+    sniff(filter="ip",iface="wlan0",prn=sniffPackets)   
+          
+          
+if __name__ == '__main__':
+          main()
